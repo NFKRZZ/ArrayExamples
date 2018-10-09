e1a1066a9621e4824c49f5b4608a328a7c4660bf
NFKRZZ <43969824+NFKRZZ@users.noreply.github.com>
2018-10-09 12:17:56 -0700

    som
    som

--- Main.java
+++ Main.java
@@ -29,17 +29,29 @@ class Main
          * Task 1.  Create an array of integers from 0 to n-1
          *          call this array ar1.  Print it out below.
          */
-        System.out.println("*** PRINTING ar1 *");
+        System.out.println("*** PRINTING ar1 ***");
         int[] ar1 = new int[n];
         for(int i=0;i<n;i++)
                  ar1[i]=i;
+                 
+         for(int i = 0; i<n; i++)
+         System.out.println("ar1["+i+"] = " +ar1[i]);
+         
+         
                 
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
         
-        
+         System.out.println("** ar2 **");
+         int[] ar2 = new int [ar1.length];
+         
+         for(int i = 0;i<ar2.length;i++)
+         ar2[i] = ar1[i];
+         ar1[3]=1000;
+         for(int i =0;i<ar2.length;i++)
+         System.out.println("ar2["+i+"] = "+ar2[i]);
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
          */
